bb625219ddbd872ec713d25ba4ff28178b58b74e
mee96 <carmenmc96@gmail.com>
2025-02-26 18:40:02 +0100

    created

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './components/App.jsx';ç
+import { HashRouter } from 'react-router-dom';
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <HashRouter> 
+             <App />
+        </HashRouter>
+    </StrictMode>,
+)
